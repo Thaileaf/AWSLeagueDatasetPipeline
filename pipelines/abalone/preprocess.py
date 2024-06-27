08b6c68163b5c87a1a3e6485ec46e841e11b4d67
Thaileaf <lethaison2019@gmail.com>
2024-06-27 02:40:04 -0400

    Shifting to unsupervised learning, removed labels, changed feature dtypes

--- pipelines/abalone/preprocess.py
+++ pipelines/abalone/preprocess.py
@@ -19,31 +19,117 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
+### We are no longer use abalone dataset, but rather the league of legends playoffs dataset. 
+### We are performing unsupervised learning to group the players together
+### https://www.kaggle.com/datasets/smvjkk/league-of-legends-lec-spring-playoffs-2024-stats?resource=download
+
+
+
+## Our new dataset has headers
 
 # Since we get a headerless CSV file we specify the column names here.
-feature_columns_names = [
-    "sex",
-    "length",
-    "diameter",
-    "height",
-    "whole_weight",
-    "shucked_weight",
-    "viscera_weight",
-    "shell_weight",
-]
-label_column = "rings"
+# feature_columns_names = [
+#     "sex",
+#     "length",
+#     "diameter",
+#     "height",
+#     "whole_weight",
+#     "shucked_weight",
+#     "viscera_weight",
+#     "shell_weight",
+# ]
+# label_column = "rings"
+
+import numpy as np
 
 feature_columns_dtype = {
-    "sex": str,
-    "length": np.float64,
-    "diameter": np.float64,
-    "height": np.float64,
-    "whole_weight": np.float64,
-    "shucked_weight": np.float64,
-    "viscera_weight": np.float64,
-    "shell_weight": np.float64,
+    "Player": str,
+    "Role": str,
+    "Team": str,
+    "Opponent_Team": str,
+    "Opponent_Player": str,
+    "Date": str,
+    "Round": int,
+    "Day": int,
+    "Patch": str,
+    "Stage": str,
+    "No_Game": int,
+    "all_Games": int,
+    "Format": int,
+    "Game_of_day": int,
+    "Side": str,
+    "Time": str,
+    "Ban": str,
+    "Ban_Opponent": str,
+    "Pick": str,
+    "Pick_Opponent": str,
+    "Champion": str,
+    "Champion_Opponent": str,
+    "Outcome": str,
+    "Kills_Team": int,
+    "Turrets_Team": int,
+    "Dragon_Team": int,
+    "Baron_Team": int,
+    "Level": int,
+    "Kills": int,
+    "Deaths": int,
+    "Assists": int,
+    "KDA": np.float64,
+    "CS": int,
+    "CS in Team's Jungle": int,
+    "CS in Enemy Jungle": int,
+    "CSM": np.float64,
+    "Golds": int,
+    "GPM": np.float64,
+    "GOLD%": np.float64,
+    "Vision Score": int,
+    "Wards placed": int,
+    "Wards destroyed": int,
+    "Control Wards Purchased": int,
+    "Detector Wards Placed": int,
+    "VSPM": np.float64,
+    "WPM": np.float64,
+    "VWPM": np.float64,
+    "WCPM": np.float64,
+    "VS%": np.float64,
+    "Total damage to Champion": int,
+    "Physical Damage": int,
+    "Magic Damage": int,
+    "True Damage": int,
+    "DPM": np.float64,
+    "DMG%": int,
+    "K+A Per Minute": np.float64,
+    "KP%": int,
+    "Solo kills": int,
+    "Double kills": int,
+    "Triple kills": int,
+    "Quadra kills": int,
+    "Penta kills": int,
+    "GD@15": int,
+    "CSD@15": int,
+    "XPD@15": int,
+    "LVLD@15": int,
+    "Objectives Stolen": int,
+    "Damage dealt to turrets": int,
+    "Damage dealt to buildings": int,
+    "Total heal": int,
+    "Total Heals On Teammates": int,
+    "Damage self mitigated": int,
+    "Total Damage Shielded On Teammates": int,
+    "Time ccing others": int,
+    "Total Time CC Dealt": int,
+    "Total damage taken": int,
+    "Total Time Spent Dead": int,
+    "Consumables purchased": int,
+    "Items Purchased": int,
+    "Shutdown bounty collected": int,
+    "Shutdown bounty lost": int
 }
-label_column_dtype = {"rings": np.float64}
+
+
+## Removing labels since we are doing unsupervised learning
+
+# label_column_dtype = {"rings": np.float64}
 
 
 def merge_two_dicts(x, y):
@@ -73,20 +159,18 @@ if __name__ == "__main__":
     logger.debug("Reading downloaded data.")
     df = pd.read_csv(
         fn,
-        header=None,
-        names=feature_columns_names + [label_column],
-        dtype=merge_two_dicts(feature_columns_dtype, label_column_dtype),
+        header=None, # FORME: What does header do?
+        dtype=feature_columns_dtype,
     )
     os.unlink(fn)
 
     logger.debug("Defining transformers.")
-    numeric_features = list(feature_columns_names)
-    numeric_features.remove("sex")
+    numeric_features = [col for col, dtype in feature_columns_dtype.items() if dtype in [int, float, np.float64]]
+    categorical_features = [col for col, dtype in feature_columns_dtype.items() if dtype == str]
     numeric_transformer = Pipeline(
         steps=[("imputer", SimpleImputer(strategy="median")), ("scaler", StandardScaler())]
     )
 
-    categorical_features = ["sex"]
     categorical_transformer = Pipeline(
         steps=[
             ("imputer", SimpleImputer(strategy="constant", fill_value="missing")),
